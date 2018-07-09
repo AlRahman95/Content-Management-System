1787f50e1e52e2f09b9e93d5a6b4f4561edf5fbc
AlRahman95 <AlRahman95@hotmail.com>
2018-07-09 19:06:41 -0400

    initialized cms project to git

---
+++ CodeKages_Database_Material/CodeKages_Database.sql
@@ -0,0 +1,111 @@
+DROP DATABASE IF EXISTS CodeKages_Database;
+
+CREATE DATABASE IF NOT EXISTS CodeKages_Database;
+
+USE CodeKages_Database;
+
+CREATE TABLE IF NOT EXISTS users (
+	userID INT(11) NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30) NOT NULL, 
+    lastName VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL, 
+    userName VARCHAR(50) NOT NULL, 
+    `password` VARCHAR(100) NOT NULL,
+    enabled boolean NOT NULL,
+    PRIMARY KEY (UserID));
+    
+CREATE TABLE IF NOT EXISTS roles (
+	roleID INT(11) NOT NULL AUTO_INCREMENT, 
+    roleType VARCHAR(30) NOT NULL, 
+    PRIMARY KEY (roleID));
+    
+CREATE TABLE IF NOT EXISTS users_roles (
+	userID INT(11), 
+    roleID INT(11),
+    PRIMARY KEY (userID, roleID),
+    FOREIGN KEY (userID)
+		REFERENCES users(userID) ON DELETE CASCADE,
+	FOREIGN KEY (roleID)
+		REFERENCES roles(roleID)  ON DELETE CASCADE);
+
+CREATE TABLE IF NOT EXISTS staticPages (
+	staticPageID INT(11) NOT NULL AUTO_INCREMENT,
+    userID INT(11) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    content LONGTEXT NOT NULL,
+    creationDate DATETIME NOT NULL,
+    PRIMARY KEY (staticPageID),
+    FOREIGN KEY (userID) 
+		REFERENCES users(userID)  ON DELETE CASCADE);
+	
+CREATE TABLE IF NOT EXISTS categories (
+	categoryID INT(11) NOT NULL AUTO_INCREMENT,
+    categoryName VARCHAR(50) NOT NULL, 
+    description VARCHAR(100) NULL,
+    PRIMARY KEY (categoryID));
+
+CREATE TABLE IF NOT EXISTS blogs (
+	blogID INT(11) NOT NULL AUTO_INCREMENT, 
+    userID INT(11) NOT NULL,
+    categoryID INT (11) NOT NULL, 
+    creationDate DATETIME NOT NULL, 
+    publishDate DATETIME NOT NULL, 
+    approvedDate DATETIME NOT NULL,
+    expirationDate DATETIME NOT NULL,
+    isApproved BOOL NOT NULL, 
+    title VARCHAR(50) NOT NULL, 
+    content LONGTEXT NOT NULL,
+    PRIMARY KEY (blogID), 
+    FOREIGN KEY (userID)
+		REFERENCES users(userID) ON DELETE CASCADE,
+	FOREIGN KEY (categoryID) 
+		REFERENCES categories(categoryID) ON DELETE CASCADE);
+
+CREATE TABLE IF NOT EXISTS tags (
+	tagID INT(11) NOT NULL AUTO_INCREMENT, 
+    tagName VARCHAR(50) NOT NULL, 
+    PRIMARY KEY (tagID));
+
+CREATE TABLE IF NOT EXISTS blogs_tags (
+	blogID INT(11) NOT NULL, 
+    tagID INT(11) NOT NULL,
+    PRIMARY KEY (blogID, tagID),
+    FOREIGN KEY (blogID)
+		REFERENCES blogs(blogID) ON DELETE CASCADE,
+	FOREIGN KEY (tagID) 
+		REFERENCES tags(tagID) ON DELETE CASCADE);
+        
+CREATE TABLE IF NOT EXISTS requestTypes (
+	requestTypeID int(11) NOT NULL auto_increment,
+    requestType varchar(50) NOT NULL,
+    PRIMARY KEY (requestTypeID));
+        
+CREATE TABLE IF NOT EXISTS requests (
+	blogID INT(11) NOT NULL,
+    requestTypeID int(11) NOT NULL,
+    userID INT(11) NOT NULL,
+    categoryID INT (11) NOT NULL, 
+    creationDate DATETIME NOT NULL, 
+    publishDate DATETIME NOT NULL, 
+    approvedDate DATETIME NOT NULL, 
+    isApproved BOOL NOT NULL, 
+    title VARCHAR(50) NOT NULL, 
+    content LONGTEXT NOT NULL,
+    expirationDate DATETIME,
+    PRIMARY KEY (blogID), 
+    FOREIGN KEY (userID)
+		REFERENCES users(userID) ON DELETE CASCADE,
+	FOREIGN KEY (categoryID) 
+		REFERENCES categories(categoryID) ON DELETE CASCADE,
+	FOREIGN KEY (requestTypeID)
+        REFERENCES requesttypes(requestTypeID) ON DELETE CASCADE
+	);
+        
+CREATE TABLE IF NOT EXISTS requests_tags (
+	blogID INT(11) NOT NULL, 
+    tagID INT(11) NOT NULL,
+    PRIMARY KEY (blogID, tagID),
+    FOREIGN KEY (blogID)
+		REFERENCES requests(blogID) ON DELETE CASCADE,
+	FOREIGN KEY (tagID) 
+		REFERENCES tags(tagID) ON DELETE CASCADE);
---
+++ CodeKages_Database_Material/codekagedummyData.sql
@@ -0,0 +1,101 @@
+USE CodeKages_Database;
+
+INSERT INTO users (firstName,lastName,email,userName,`password`,enabled) 
+VALUES 
+('Jesse','Azu','jesseAzu@gmail','Admin','$2a$10$qAL9f5LyXQoTHqElqjuiXuANUyTW.4IAavEMn9fu3gfrx4YH7nUj2',1),
+('Al','Rahman','alRahman@gmail.com','booBlogger','$2a$10$qAL9f5LyXQoTHqElqjuiXuANUyTW.4IAavEMn9fu3gfrx4YH7nUj2',1),
+('Rich','Tav','taverasRich@gmail.com','anbuBlogger','$2a$10$qAL9f5LyXQoTHqElqjuiXuANUyTW.4IAavEMn9fu3gfrx4YH7nUj2',1)
+;
+
+INSERT INTO roles(roleType)
+VALUES
+('ROLE_ADMIN'),
+('ROLE_USER')
+;
+
+INSERT INTO users_roles (userID,roleID)
+VALUES
+('1','1'),
+('1','2'),
+('2','2'),
+('3','2')
+;
+
+INSERT INTO categories (categoryName,description)
+VALUES
+('Art','Ranging from vintage to modern art'),-- 1
+('Business','Regarding the latest in business'),-- 2
+('Technology','Up to date info on todays technology'),-- 3
+('Nature','Current news on nature'),-- 4
+('Politics','Exciting news on politics')-- 5
+;
+
+INSERT INTO blogs (userID,categoryID,creationDate,publishDate,approvedDate,isApproved,title,content, expirationDate)
+VALUES
+('3','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','Gadgets and Robots','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+<<<<<<< HEAD
+('3','1','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','A','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','2','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','B','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','2','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','C','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','4','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','D','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','4','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','0','E','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','1','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','F','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','G','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','2','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','0','H','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','2','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','I','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('1','2','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','J','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('1','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','0','K','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('1','4','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','L','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','4','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','M','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('1','4','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','N','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','4','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','0','O','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','1','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','0','P','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('1','1','2018-05-30 23:59:59','2018-06-30 23:59:59','2018-05-31 23:59:59','1','Q','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('1','2','2018-05-30 23:59:59','2018-04-30 23:59:59','2018-05-31 23:59:59','1','R','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('3','5','2018-05-30 23:59:59','2018-07-30 23:59:59','2018-05-31 23:59:59','1','S','Some stuff about gadgets and robots','2999-05-31 23:59:59')
+=======
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','A','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','B','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','C','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','D','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','0','E','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','F','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','G','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','0','H','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-30 23:59:59','1','I','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-02-20 23:59:59','1','J','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-30 23:59:59','0','K','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-03-30 23:59:59','1','L','Some stuff about gadgets and robots','2999-05-31 23:59:59'),
+('2','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-04-30 23:59:59','1','M','Some stuff about gadgets and robots','2999-05-31 23:59:59')
+>>>>>>> c2c1c1302b6a66703eea97e5d9a9900bf0881481
+;
+
+INSERT INTO requesttypes (requestType)
+VALUES
+('edit'),
+('delete')
+;
+
+/*
+INSERT INTO requests (userID,categoryID,creationDate,publishDate,approvedDate,isApproved,title,content, expirationDate, requestTypeID, blogID)
+VALUES
+('3','3','2018-05-30 23:59:59','2018-05-31 23:59:59','2018-05-31 23:59:59','1','Gadgets and Robots and Death Metal','Some stuff about gadgets and robots','2999-05-31 23:59:59', '2', '1');
+*/
+
+INSERT INTO staticPages (userID, title, content, creationDate)
+VALUES
+('3','testPage','testPage Content', '2018-05-30 23:59:59');
+
+
+INSERT INTO tags (tagName)
+VALUES
+('Gadgetry'),
+('modernArt'),
+('robots')
+;
+
+INSERT INTO Blogs_Tags (blogID,tagID)
+VALUES
+('1','1'),
+('1','3')
+;
